5f02f94ed61d6624eaf373ed6baa284a48b2a685
dandipietrantonio <dd2742@nyu.edu>
2019-06-12 21:46:49 -0400

    complex class v1 and added image to weather

---
+++ my_code/complex.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <istream>
+#include <iomanip>
+#include "complex.h"
+
+using namespace std;
+
+//Constructor
+Complex::Complex(double r, double i) :
+    real(r), imag(i) {}
+
+//Input to create a Complex Number
+istream& operator>> (istream& is, Complex& c) {
+    is >> c.real >> c.imag;
+    return is;
+}
+
+//Output
+ostream& operator<< (ostream& os, const Complex& c) {
+    /*
+     * Outputting a `Complex` instance, while illustrating some of the
+     * capabilities of I/O streams: `setprecision` gives us a fixed
+     * number of decimal places, while `showpos` turns on the plus
+     * sign for positive numbers.
+     * */
+    os << fixed << setprecision(1) << c.get_real() << " + " << 
+    fixed << setprecision(1) << c.get_imag() << "i";
+    return os;
+}
+
+//Comparison Overloading
+bool operator== (const Complex& c1, const Complex& c2) {
+    double margin = 0.001; //wasn't working with direct comparison, I believe due to c++ error
+    if (abs(c1.get_real() - c2.get_real()) <= margin && abs(c1.get_imag() - c2.get_imag()) <= margin) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Boolean Overloading
+Complex::operator bool() const {
+    if (real != 0 || imag != 0) {return true;}
+    else {return false;}
+}
+
+//++c Overloading
+Complex& Complex::operator++() {
+    real += 1;
+    return *this;
+}
+
+//c++ Overloading
+Complex Complex::operator++(int dummy) { // how do i return the old value with *this???????
+    Complex val_before = *this;
+    real += 1;
+    return val_before;
+}
+
+//--c Overloading
+Complex& Complex::operator--() {
+    real -= 1;
+    return *this;
+}
+
+//c-- Overloading
+Complex Complex::operator--(int dummy) { // how do i return the old value with *this???????
+    Complex val_before = *this;
+    real -= 1;
+    return val_before;
+}
+
+
+//Gets real number portion
+double Complex::get_real() const {
+    return real;
+}
+
+//Gets imaginary portion
+double Complex::get_imag() const {
+    return imag;
+}
+
+//Multiplies by a scalar
+Complex Complex::operator*(const int i) {
+    real *= i;
+    imag *= i;
+    return *this;
+}
+
+//Subtracts by another complex number
+ Complex Complex::operator-(const Complex& c){
+     return Complex(real - c.get_real(), imag - c.get_imag());
+ }
+
+//Subtracts by another number and assigns difference to self
+Complex Complex::operator-=(const Complex& c){
+    real -= c.get_real();
+    imag -= c.get_imag();
+    return *this;
+}
+
+//Addition with another complex number
+ Complex Complex::operator+(const Complex& c){
+     return Complex(real + c.get_real(), imag + c.get_imag());
+ }
+
--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -5,19 +5,66 @@
 
 using namespace std;
 
+
+Image::Image(int w, int h, std::string flnm)
+    : width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+    copy_fields(img2);
+}
+
+Image::~Image() {
+    if (image_buf != nullptr) {delete image_buf;}
+}
+
+Image& Image::operator=(const Image& img2) {
+    if (image_buf != nullptr) {delete image_buf;}
+    copy_fields(img2);
+    return *this;
+ }
+
+int Image::image_sz() const {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+    height = img2.height;
+    width = img2.width;
+    filename = img2.filename;
+    image_buf = new char[img2.image_sz()];
+    for (int i = 0; i < img2.image_sz(); ++i) {
+        image_buf[i] = img2.image_buf[i];
+    }
+}
+
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
 //Constructor for Weather Class
 Weather::Weather(string nm, GPS loc) :
     station_nm(nm), my_loc(loc) {
 }
 
-//Output for Weather Class
-ostream& operator<<(ostream& os, const Weather& w){
-    os << w.get_name() << " rating: " << w.get_rating() << "; " << w.my_loc << endl;
-    for (WReading wreading: w.wreadings) { 
-        os << wreading << endl;
-    }
-    return os;
-}
+//Output for Weather Class COMMENTED OUT FOR TESTING
+// ostream& operator<<(ostream& os, const Weather& w){
+//     os << w.get_name() << " rating: " << w.get_rating() << "; " << w.my_loc << endl;
+//     for (WReading wreading: w.wreadings) { 
+//         os << wreading << endl;
+//     }
+//     return os;
+// }
 
 //Output for GPS Class
 ostream& operator<<(ostream& os, const GPS& gps){
@@ -25,11 +72,11 @@ ostream& operator<<(ostream& os, const GPS& gps){
     return os;
 }
 
-//Output for WReading Class
-ostream& operator<<(ostream& os, const WReading& wr){
-    os << wr.date << " " << wr.temperature << " " << wr.humidity << " " << wr.windspeed;
-    return os;
-}
+//Output for WReading Class - TEMP. COMMENTED OUT FOR TESTING
+// ostream& operator<<(ostream& os, const WReading& wr){
+//     os << wr.date << " " << wr.temperature << " " << wr.humidity << " " << wr.windspeed;
+//     return os;
+// }
 
 //Methods for Weather Class
     string Weather::get_name() const {
