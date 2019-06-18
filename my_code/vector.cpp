b52af804e1a903c9c08178d02f6802c66806be86
dandipietrantonio <dd2742@nyu.edu>
2019-06-18 13:01:15 -0400

    vector class v1

---
+++ my_code/vector.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include "vector.h"
+
+using namespace std;
+
+//Prints the Vector Element by Element
+    void print_vector(const MyVec& v) {
+        for (int i=0; i < v.size(); ++i) {
+            cout << v[i] << ' ';
+        }
+    }
+
+//Default Constructor for MyVec
+    MyVec::MyVec() {
+        sz = 0;
+        capacity = 1;
+        data = new int[capacity];
+    }
+
+//Copy Constructor for MyVec
+    MyVec::MyVec(const MyVec& v2) {
+        sz = v2.sz;
+        capacity = v2.capacity;
+        data = new int[capacity];
+        for (int i=0; i < capacity; ++i) {
+            data[i] = v2.data[i];
+        }
+    }
+
+//Destructor for MyVec
+    MyVec::~MyVec() {
+        delete[] data;
+    }
+
+//Operator Overload: =
+    MyVec& MyVec::operator=(const MyVec& v2) {
+        delete[] data;
+        sz = v2.sz;
+        capacity = v2.capacity;
+        data = new int[v2.capacity];
+        for (int i=0; i < v2.capacity; ++i) {
+            data[i] = v2.data[i];
+        }
+        return *this;
+
+    }
+
+//Operator Overload: ==
+    bool operator==(MyVec& v1, MyVec& v2) {
+        if (v1.size() != v2.size()) {
+            return false;
+        }
+        else {
+            for (int i=0; i < v1.size(); ++i) {
+                if (v1[i] != v2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+//Pushes Element to the Back of the Vector
+    void MyVec::push_back(int val) {
+        if (sz == capacity) {
+            int* old_data = data;
+            capacity *= 2;
+            data = new int[capacity];
+            for (int i = 0; i < sz; ++i) {
+                data[i] = old_data[i];
+            }
+            delete[] old_data;
+        }
+        data[sz++] = val;
+    }
+
+//Operator Overload: []
+    int MyVec::operator[](int i) const {
+        return data[i];
+    }
+
+//Operator Overload: [] by Reference
+    int& MyVec::operator[](int i) {
+        return data[i];
+    }
